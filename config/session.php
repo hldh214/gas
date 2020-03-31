b855aaebd5989d13b9e03d60fa2d436781c1e7f5
hldh214 <hldh214@gmail.com>
2020-03-31 10:40:02 +0800

    build(composer): bump version
    Laravel7

--- config/session.php
+++ config/session.php
@@ -164,7 +164,7 @@ return [
     |
     */
 
-    'secure' => env('SESSION_SECURE_COOKIE', false),
+    'secure' => env('SESSION_SECURE_COOKIE', null),
 
     /*
     |--------------------------------------------------------------------------
@@ -192,6 +192,6 @@ return [
     |
     */
 
-    'same_site' => null,
+    'same_site' => 'lax',
 
 ];
