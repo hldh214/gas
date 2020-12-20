2981b3c7311d5af03ba713959a5ee0b5b19fe4d8
hldh214 <hldh214@gmail.com>
2020-12-20 21:35:20 +0800

    feat(main): add laravel-wechat

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\JLibController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::any('/JLib', [JLibController::class, 'index']);
